0a4793b9450bf14fc625f51be4371883465d8413
lorna-chepkirui <lornachepkirui20@gmail.com>
2022-08-22 23:02:51 +0300

    commit tree 2460af823fd4ae905ecf435201d00c3a3bf4f181

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,26 @@
 # Write your code here.
+def line(customers)
+    people = ""
+    if !customers.empty?
+      cust = customers.map.with_index do |customer, i|
+          "#{i+1}. #{customer}"
+      end
+      puts cust.join(" ").prepend("The line is currently: ")
+    else
+      puts "The line is currently empty."
+    end
+  
+  end
+  
+  def take_a_number(customers, names)
+    customers << names
+    puts "Welcome, #{names}. You are number #{customers.size} in line."
+  end
+  
+  def now_serving(customers)
+    if customers.empty?
+      puts "There is nobody waiting to be served!"
+    else
+      puts "Currently serving #{customers.shift}."
+    end
+  end
